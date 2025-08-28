582ec49d1b70f6e2b9a600dbb63c6caeab62bd54
lithikkumar <kumarlithik534@gmail.com>
2025-08-28 13:25:17 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=4;
+	int b=7;
+	clrscr();
+	printf("addition of 4+7=%d\n",a+b);
+	printf("sunbtraction of 4-7=%d\n",a-b);
+	printf("multipaction of 4*7=%d\n",a*b);
+	printf("dividion of 4/7=%d\n",a/b);
+	getch();
+}
